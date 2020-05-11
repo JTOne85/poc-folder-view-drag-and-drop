de846c739e056e32c3829cc4f4af052770e485c4
JTOne85 <terblanche.jacques@gmail.com>
2020-05-11 16:21:19 +0200

    3 files modified

--- src/app/folder-view/folder-view.component.ts
+++ src/app/folder-view/folder-view.component.ts
@@ -1,76 +1,111 @@
-import { Component } from '@angular/core';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
-import { FlatTreeControl } from '@angular/cdk/tree';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener
+} from "@angular/material/tree";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
-export class FolderNode implements IFolderNode{
-  constructor(public id: string,
-  public name: string,
-  public children?: FolderNode[]){}
+export class FolderNode implements IFolderNode {
+  constructor(
+    public id: string,
+    public name: string,
+    public children?: FolderNode[]
+  ) {}
 }
 
-interface IFolderNode{
-  id: string,
-  name: string,
-  children?: FolderNode[]
+interface IFolderNode {
+  id: string;
+  name: string;
+  children?: FolderNode[];
 }
 const TREE_DATA = [
   {
-    id:'1',
-    name: 'Root folder',
+    id: "1",
+    name: "Root folder",
     children: [
       {
-        id: '2',
-        name: 'first level',
+        id: "2",
+        name: "first level",
         children: [
-          {id: '3', name: 'first child'},
-          {id: '4', name: 'first sibling'},
-          {id: '5', name: 'second sibling '}
-          ]
+          { id: "3", name: "first child" },
+          { id: "4", name: "first sibling" },
+          { id: "5", name: "second sibling " }
+        ]
       },
       {
-        id: '6',
-        name: 'first level sibling',
+        id: "6",
+        name: "first level sibling",
         children: [
-          {id: '7', name: 'first child'},
-          {id: '8', name: 'first sibling'},
-          {id: '9', name: 'second sibling', children:[
-            {id: '3', name: 'first child'},
-          {id: '4', name: 'first sibling'},
-          {id: '5', name: 'second sibling '}
-          ]}
-          ]
-        }
+          { id: "7", name: "first child" },
+          { id: "8", name: "first sibling" },
+          {
+            id: "9",
+            name: "second sibling",
+            children: [
+              { id: "3", name: "first child" },
+              { id: "4", name: "first sibling" },
+              { id: "5", name: "second sibling " }
+            ]
+          }
+        ]
+      }
     ]
   }
 ];
 
-interface FolderFlatNode{
+interface FolderFlatNode {
   expandable: boolean;
   name: string;
   level: number;
+  id: string;
 }
 
 @Component({
-  selector: 'folder-view',
-  templateUrl: './folder-view.component.html',
-  styleUrls: [ './folder-view.component.css' ]
+  selector: "folder-view",
+  templateUrl: "./folder-view.component.html",
+  styleUrls: ["./folder-view.component.css"]
 })
-export class FolderViewComponent  {
+export class FolderViewComponent {
+  @Input() item: FolderFlatNode;
+  @Input() parentItem?: FolderFlatNode;
+  @Input() public set connectedDropListsIds(ids: string[]) {
+    this.allDropListsIds = ids;
+  }
+
+  public allDropListsIds: string[];
+  public get dragDisabled(): boolean {
+    return !this.parentItem;
+  }
+
+  public get parentItemId(): string {
+    return this.dragDisabled ? "" : this.parentItem.id
+  }
 
   private _transformer = (node: FolderNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
       name: node.name,
-      level: level,
+      level: level
     };
-  }
+  };
 
-  constructor(){
+  @Output() itemDrop: EventEmitter<CdkDragDrop<FolderFlatNode>>
+
+  constructor() {
     this.dataSource.data = TREE_DATA;
   }
 
-  treeFlattener = new MatTreeFlattener(this._transformer, node => node.level, node => node.expandable, node => node.children);
-  treeControl = new FlatTreeControl<FolderFlatNode>(node => node.level, node => node.expandable);
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  );
+  treeControl = new FlatTreeControl<FolderFlatNode>(
+    node => node.level,
+    node => node.expandable
+  );
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   hasChild = (_: number, node: FolderFlatNode) => node.expandable;
