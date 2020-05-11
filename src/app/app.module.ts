d14de076356792bfdb4cc5cc001911bcc6a48e5b
JTOne85 <terblanche.jacques@gmail.com>
2020-05-11 15:10:14 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatIconModule } from "@angular/material/icon";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { FolderViewComponent} from './folder-view/folder-view.component';
-
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { FolderViewComponent } from "./folder-view/folder-view.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatTreeModule, MatIconModule],
-  declarations: [ AppComponent, HelloComponent, FolderViewComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MatTreeModule, MatIconModule, DragDropModule],
+  declarations: [AppComponent, HelloComponent, FolderViewComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
